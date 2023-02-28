8192224d90a99cf42f1a871f89fe401a391d5e6e
miyuk <enough7531@gmail.com>
2023-03-01 03:56:21 +0900

    Feature add vlan group viewer (#2)
    * add docker develop directory
    
    * publish v0.0.1
    
    * Create LICENSE
    
    * delete unused template code

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # NetBox VLAN Manager
+
+NetBox plugin for viewer of multiple VLAN Group spaces.
+
+## Features
+
+This plugin provide follwing Models:
+
+- VLAN Group Set
+
+## Compatibility
+
+Each Plugin Version listed below has been tested with its corresponding NetBox Version.
+
+| NetBox Version | Plugin Version |
+| :------------: | :------------: |
+|      3.4       |     0.0.1      |
+
+## Installation
+
+The plugin is available as a Python package in pypi and can be installed with pip:
+
+```bash
+pip install netbox-vlan-manager
+```
+
+Enable the plugin in /opt/netbox/netbox/netbox/configuration.py
+
+```python
+PLUGINS = ['netbox-vlan-manager']
+```
+
+Restart NetBox
+
+## Configuration
+
+Currently, This plugin is not necessary plugin configuration
+
+## Screenshots
+
+VLAN Group Set List
+![VLAN Group Set List](docs/img/vlan_group_set_list.png)
+
+VLAN Group View Set with VLANs
+![VLAN Group Set VLANs](docs/img/vlan_group_set_vlans.png)
--- netbox_vlan_manager/views.py
+++ netbox_vlan_manager/views.py
@@ -1,10 +1,29 @@
 from django.db.models import Count
 from netbox.views import generic
 from . import models, tables, forms
+from ipam.models import VLAN
+from ipam.utils import add_available_vlans
+from ipam.tables import VLANTable
 
 
 class VLANGroupSetView(generic.ObjectView):
     queryset = models.VLANGroupSet.objects.all()
+
+    def get_extra_context(self, request, instance):
+        vlan_groups = instance.vlan_groups.all()
+        max_vid = max(vlan_groups, key=(lambda x: x.max_vid)).max_vid
+        min_vid = min(vlan_groups, key=(lambda x: x.min_vid)).min_vid
+
+        vlan_group_vlans = {}
+        for vid in range(min_vid, max_vid + 1):
+            vlan_group_vlans[vid] = {}
+            for vlan_group in vlan_groups:
+                vlans = VLAN.objects.filter(group=vlan_group, vid=vid)
+                vlan_group_vlans[vid][vlan_group.id] = vlans[0] if vlans else None
+
+        return {
+            'vlan_group_vlans': vlan_group_vlans
+        }
 
 
 class VLANGroupSetListView(generic.ObjectListView):
