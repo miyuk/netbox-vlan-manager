565edb2c4a3bf70f56ac05f58b468bfb02f0b93a
miyuk <enough7531@gmail.com>
2024-05-24 08:48:11 +0900

    change extra.plugins to netbox.plugins

--- netbox_vlan_manager/navigation.py
+++ netbox_vlan_manager/navigation.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginMenuItem, PluginMenuButton, PluginMenuItem
+from netbox.plugins import PluginMenuItem, PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 vlan_group_manager_buttons = [
